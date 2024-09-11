f736dc07d7fcba81413116de0a42f6647c88638f
Jesus Zermeño <josezerrod@hotmail.com>
2024-09-11 13:34:19 -0600

    Refactor program data: Add speakers and their profiles for the session on electromobility regulations

--- src/data/speakers.ts
+++ src/data/speakers.ts
@@ -1,5 +1,4 @@
 interface Speaker {
-    id: number;
     name: string;
     photo: string;
     profile?: string[];
@@ -15,6 +14,26 @@ interface Speaker {
     }
 */
 
-const speaker : Speaker[] = []
+const speaker: Speaker[] = [
+
+    {
+        name: "Ramón Alfaro",
+        photo: "/speakers/ramon_alfaro_seg.webp",
+        profile: [
+            "Es Licenciado en Relaciones Internacionales por la Universidad Nacional Autónoma de México (UNAM). Su trayectoria incluye cargos municipales en la ciudad de León y otros puestos dentro del sector público estatal.",
+            "Como algunas de sus actividades sobresalientes destaca el haber sido responsable de crear la Subsecretaría de Apoyo a las Micro, Pequeñas y Medianas Empresas, líder de programas como Mi Plaza y En Marcha; entre otras. Antes de su nuevo nombramiento se desempeñó como subsecretario de empleo y formación laboral."
+        ],
+        position: "Secretario de Desarrollo Económico Sustentable de Guanajuato"
+    },
+    {
+        name: "Odracir Barquera",
+        photo: "/speakers/odracir_barquera.webp",
+        profile: [
+            "Es Director General de la Asociación Mexicana de la industria Automotriz (AMIA). Previamente, fue Director General de Estrategia en la Presidencia del Consejo Coordinador Empresarial, a cargo del área internacional, el US Mexico CEO Dialogue, la oficina de Washington, Dimensión Social y la Agenda 2030. Antes fue Consultor Independiente de empresas privadas y organismos multilaterales sobre políticas públicas, estrategias sociales, asuntos políticos e internacionales; Asesor Especial del Presidente de la Cámara de Diputados para la institucionalización, diseño e implementación del Sistema de Evaluación Legislativa; Coordinador de Asesores de la Senadora Cristina Díaz(Pdta.de la Comisión de Gobernación); Director de Enlace Institucional y CoPresidente del Comité de Asuntos Fiscales de ConMéxico(Consejo Mexicano de la Industria de Productos de Consumo); y en la Oficina del Banco Mundial para México y Colombia coordinó Vinculación Interinstitucional y Análisis Político, participando en diversos estudios y proyectos especiales sobre Pobreza, Gobernabilidad y Política Económica; entre otros cargos."
+        ],
+        position: "Director General de la Asociación Mexicana de la industria Automotriz (AMIA)."
+    }
+
+]
 
 export default speaker;
