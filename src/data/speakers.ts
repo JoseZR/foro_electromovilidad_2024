93d87dc32e82ef5d8a8b4922eb79690ae1392f26
Jesus Zermeño <josezerrod@hotmail.com>
2024-09-17 18:10:45 -0600

    Refactor speaker data: Update Elisa Crespo's position in Cluster Automotriz Regional

--- src/data/speakers.ts
+++ src/data/speakers.ts
@@ -52,47 +52,60 @@ const speaker: Speaker[] = [
   {
     name: "Elisa Crespo",
     photo: "/speakers/elisa_crespo.webp",
-    profile: ["Ejecutiva del Clúster Automotriz Metropolitano A.C., con 24 años de experiencia en la industria automotriz y como asesora de estrategias de negocio para diversos estados y asociaciones del sector. Es Licenciada en Mercadotecnia por la UNAM y Maestra en Alta Dirección por el IPADE. Ha recibido múltiples reconocimientos, incluyendo el Assembly Siemens en dos ocasiones consecutivas por su contribución a la Transformación Digital en México y el Premio Nacional a la Calidad Automotriz. Es una destacada promotora y desarrolladora del Business Automotive Meeting (BAM), un evento clave para el desarrollo de la cadena de proveeduría del sector automotriz, que ha celebrado más de 10 ediciones. Actualmente, continúa impulsando proyectos enfocados en la Electromovilidad."],
-    position: "Presidenta Ejecutiva en Cluster Automotriz Regional de Hidalgo, Ciudad de México y Estado de México",
+    profile: [
+      "Ejecutiva del Clúster Automotriz Metropolitano A.C., con 24 años de experiencia en la industria automotriz y como asesora de estrategias de negocio para diversos estados y asociaciones del sector. Es Licenciada en Mercadotecnia por la UNAM y Maestra en Alta Dirección por el IPADE. Ha recibido múltiples reconocimientos, incluyendo el Assembly Siemens en dos ocasiones consecutivas por su contribución a la Transformación Digital en México y el Premio Nacional a la Calidad Automotriz. Es una destacada promotora y desarrolladora del Business Automotive Meeting (BAM), un evento clave para el desarrollo de la cadena de proveeduría del sector automotriz, que ha celebrado más de 10 ediciones. Actualmente, continúa impulsando proyectos enfocados en la Electromovilidad.",
+    ],
+    position:
+      "Presidenta Ejecutiva en Cluster Automotriz Regional de Hidalgo, Ciudad de México y Estado de México",
   },
   {
-      name: "Vidaur Mora",
-      photo: "/speakers/vidaur_mora.webp",
-      profile: [
-          "Socio patrimonial de la firma Rivadeneyra Treviño, se especializa en Cross Border Investment y  Comercio Exterior; enfocado principalmente en atraer y asesorar inversión extranjera en México en sus procesos legales de establecimiento,  instalación y operación; para ello ha colaborado exitosamente en un importante número de proyectos de inversión para la instalación de plantas productivas en el sector automotriz, manufactura industrial, textil, agroalimentos, entre otros,  en diferentes estados de la República Mexicana. Es miembro activo del Clúster Automotriz Zona Centro.  Miembro de la IPBA (Inter-Pacific Bar Association) perteneciendo a su comité Cross Border Investment & International Trade."],
-      position: "Especialista en Cross Border Investment y Comercio Exterior",
+    name: "Vidaur Mora",
+    photo: "/speakers/vidaur_mora.webp",
+    profile: [
+      "Socio patrimonial de la firma Rivadeneyra Treviño, se especializa en Cross Border Investment y  Comercio Exterior; enfocado principalmente en atraer y asesorar inversión extranjera en México en sus procesos legales de establecimiento,  instalación y operación; para ello ha colaborado exitosamente en un importante número de proyectos de inversión para la instalación de plantas productivas en el sector automotriz, manufactura industrial, textil, agroalimentos, entre otros,  en diferentes estados de la República Mexicana. Es miembro activo del Clúster Automotriz Zona Centro.  Miembro de la IPBA (Inter-Pacific Bar Association) perteneciendo a su comité Cross Border Investment & International Trade.",
+    ],
+    position: "Especialista en Cross Border Investment y Comercio Exterior",
   },
   {
-      name: "Asael Colunga",
-      photo: "/speakers/asaelc_colunga.webp",
-      profile: [
-          "Es Licenciado en Comercio Exterior y Aduanas, con una Maestría en Mercadotecnia. Con dos décadas de trayectoria en comercio internacional, ha adquirido experiencia en diversos sectores como calzado, automotriz, autopartes, metalmecánica y su proveeduría. Ha liderado equipos de trabajo en la promoción de la oferta exportable de los sectores productivos de Guanajuato, organizando ferias, misiones y eventos internacionales de renombre. Actualmente, dirige el área de Internacionalización en la COFOCE, organismo estatal encargado de la promoción de las exportaciones."
-      ],
-      position: "Director de Internacionalización de COFOCE"
+    name: "Asael Colunga",
+    photo: "/speakers/asaelc_colunga.webp",
+    profile: [
+      "Es Licenciado en Comercio Exterior y Aduanas, con una Maestría en Mercadotecnia. Con dos décadas de trayectoria en comercio internacional, ha adquirido experiencia en diversos sectores como calzado, automotriz, autopartes, metalmecánica y su proveeduría. Ha liderado equipos de trabajo en la promoción de la oferta exportable de los sectores productivos de Guanajuato, organizando ferias, misiones y eventos internacionales de renombre. Actualmente, dirige el área de Internacionalización en la COFOCE, organismo estatal encargado de la promoción de las exportaciones.",
+    ],
+    position: "Director de Internacionalización de COFOCE",
   },
   {
-      name: "Fermín Fernando",
-      photo: "/speakers/fermin_fernando.webp",
-      profile: [
-          "Líder consultor en proyectos con empresas multinacionales en temas como aseguramiento de calidad, optimización de procesos productivos y logística, administración de proyectos en empresas de servicios entre otros. Más de 10 años de experiencia en implementación de metodologías de Administración de proyectos. Ha trabajado en la Dirección de Planeación y Evaluación de la Secretaría de Desarrollo Urbano y Medio Ambiente del Gobierno del estado de Tamaulipas. Es instructor y consultor líder para programas y diplomados de Educación Continua en México, Ecuador, Colombia y Chile entre otros. Actualmente es Director Institucional de TEC Mobility Solutions siendo esta, la iniciativa automotriz y de la movilidad de la Vicerrectoría de Educación Continua del TEC de Monterrey. "],
-      position: "Director Tec Mobility Solutions Vicerrectoría de Educación Continua"
-  },
-   {
-      name: "Carlos Talamantes",
-      photo: "/speakers/carlos_talamantes.webp",
-      profile: [
-          "Egresado en Ingeniería Industrial por el Instituto Tecnológico de Estudios Superiores de Monterrey – Campus Saltillo, cuenta también con una Maestría en Ingeniería, Calidad y Productividad por la institución. Desde hace 15 años colabora en la filial mexicana de la empresa alemana Witzenmann, de la que actualmente es Director General. Anteriormente colaboró con la empresa de tractores John Deere por 17 años. Bajo su dirección, Witzenmann México logró el reconocimiento como uno de los 10 Mejores Lugares para Trabajar en la Región del Bajío. Witzenmann México es una empresa internacional de origen alemán, que se dedica a la producción de flexibles metálicos para los sistemas de escape de los autos y próximamente tuberías metálicas especializadas. Witzenmann cuenta con 4,500 empleados a nivel mundial, 200 de ellos en su filial de Apaseo."
-      ],
-      position: "Presidente del Cluster Automotriz de Guanajuato - CLAUGTO"
+    name: "Fermín Fernando",
+    photo: "/speakers/fermin_fernando.webp",
+    profile: [
+      "Líder consultor en proyectos con empresas multinacionales en temas como aseguramiento de calidad, optimización de procesos productivos y logística, administración de proyectos en empresas de servicios entre otros. Más de 10 años de experiencia en implementación de metodologías de Administración de proyectos. Ha trabajado en la Dirección de Planeación y Evaluación de la Secretaría de Desarrollo Urbano y Medio Ambiente del Gobierno del estado de Tamaulipas. Es instructor y consultor líder para programas y diplomados de Educación Continua en México, Ecuador, Colombia y Chile entre otros. Actualmente es Director Institucional de TEC Mobility Solutions siendo esta, la iniciativa automotriz y de la movilidad de la Vicerrectoría de Educación Continua del TEC de Monterrey. ",
+    ],
+    position:
+      "Director Tec Mobility Solutions Vicerrectoría de Educación Continua",
   },
   {
-      name: "Carlos Gutiérrez",
-      photo: "/speakers/carlos_gutierrez.webp",
-      profile: [
-          "Director de Asuntos Gubernamentales y Externos de BMW Group México. Responsable de comunicación política y representante de la empresa ante entidades gubernamentales y no gubernamentales, manejo de políticas públicas sobre sustentabilidad, regulaciones federales y estatales, tratados comerciales y responsabilidad social corporativa. Con una trayectoria profesional de más de 30 años, se ha desempeñado en diversas posiciones dentro de la Industria Automotriz en áreas como Comercio Internacional, Cadena de Suministro, Logística y Relaciones Institucionales. Ha sido consultor para diferentes proyectos de inversión extranjera, negocios internacionales y tratados comerciales en empresas relacionadas con la industria automotriz, es miembro del consejo de la AMIA."
-      ],
-      position: "Director de Asuntos Gubernamentales y Externos de BMW Group México"
-  }
+    name: "Carlos Talamantes",
+    photo: "/speakers/carlos_talamantes.webp",
+    profile: [
+      "Egresado en Ingeniería Industrial por el Instituto Tecnológico de Estudios Superiores de Monterrey – Campus Saltillo, cuenta también con una Maestría en Ingeniería, Calidad y Productividad por la institución. Desde hace 15 años colabora en la filial mexicana de la empresa alemana Witzenmann, de la que actualmente es Director General. Anteriormente colaboró con la empresa de tractores John Deere por 17 años. Bajo su dirección, Witzenmann México logró el reconocimiento como uno de los 10 Mejores Lugares para Trabajar en la Región del Bajío. Witzenmann México es una empresa internacional de origen alemán, que se dedica a la producción de flexibles metálicos para los sistemas de escape de los autos y próximamente tuberías metálicas especializadas. Witzenmann cuenta con 4,500 empleados a nivel mundial, 200 de ellos en su filial de Apaseo.",
+    ],
+    position: "Presidente del Cluster Automotriz de Guanajuato - CLAUGTO",
+  },
+  {
+    name: "Carlos Gutiérrez",
+    photo: "/speakers/carlos_gutierrez.webp",
+    profile: [
+      "Director de Asuntos Gubernamentales y Externos de BMW Group México. Responsable de comunicación política y representante de la empresa ante entidades gubernamentales y no gubernamentales, manejo de políticas públicas sobre sustentabilidad, regulaciones federales y estatales, tratados comerciales y responsabilidad social corporativa. Con una trayectoria profesional de más de 30 años, se ha desempeñado en diversas posiciones dentro de la Industria Automotriz en áreas como Comercio Internacional, Cadena de Suministro, Logística y Relaciones Institucionales. Ha sido consultor para diferentes proyectos de inversión extranjera, negocios internacionales y tratados comerciales en empresas relacionadas con la industria automotriz, es miembro del consejo de la AMIA.",
+    ],
+    position:
+      "Director de Asuntos Gubernamentales y Externos de BMW Group México",
+  },
+  {
+    name: "Azul Ogazón",
+    photo: "/speakers/azul_ogazon.webp",
+    profile: ["Con más de 10 años de experiencia en Negocios Internacionales, Azul Ogazón se ha desempeñado como Directora de Proyectos, liderando eventos como Industrial Transformation Mexico, Americas’ Mobility of the Future y MEM Industrial. Actualmente cuenta con el cargo de Subdirectora General en IGECo Mexico, donde supervisa y coordina las actividades de las diversas ferias organizadas por la misma institución, así como la participación de México en las ferias de Deutsche Messe e Italian Exhibition Group. Antes de incorporarse a IGECo Mexico colaboró en ProMéxico en el área de la Alianza del Pacífico, dirigió la representación de ProMéxico en el Norte de California y laboró en la oficina de representación de Singapur. Tiene una Maestría en Políticas Públicas, con especialización en Análisis y Políticas Económicas, de la Escuela de Políticas Públicas Lee Kuan Yew, en la Universidad Nacional de Singapur, y una Licenciatura en Economía del Centro de Investigación y Docencia Económicas (CIDE), México."], 
+    position: "Subdirectora General en Italian German Exhibition Company Mexico",
+  },
   // {
   //     name: "",
   //     photo: "/speakers/elisa_crespo.webp",
